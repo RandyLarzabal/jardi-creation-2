bce03ff915df511b08fd290787dbe8e99c158a21
randy <pro@randylarzabal.fr>
2022-09-19 20:51:44 +0200

    Initial commit

---
+++ wp-content/themes/jardicreation/assets/js/cookies.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function () {
+    jQuery.cookieBar({
+        message: 'En poursuivant votre navigation sur ce site, vous acceptez l\'utilisation de cookies pour réaliser des statistiques de visites et améliorer votre expérience d\'utilisateur.<a href="https://www.stratetcom.fr/news51" target="_blank" rel="nofollow noopener noreferrer"> En savoir plus sur le paramétrage des Cookies</a>',
+        acceptButton: true,
+        acceptText: 'J\'ai compris'
+    })
+});
